b9d0f214eb467cb47b1819fa74865d3703d5a776
AbinashChetia <abinashdu2000@gmail.com>
2023-09-25 19:52:43 +0530

    Updated

--- LogRegAC/logRegAC.py
+++ LogRegAC/logRegAC.py
@@ -1,14 +1,59 @@
 import numpy as np
 
 class LogReg:
-    def __init__(self, lr=0.01, max_iter=1000, eps=1e-5):
+    def __init__(self, lr=0.01, max_iter=1000, eps=1e-5, stochGd=False):
         self.w = None
         self.cost_hist = []
         self.w_hist = []
         self.lr = lr
         self.max_iter = max_iter
         self.eps = eps
+        self.stochGd = stochGd
+    
+    def fit(self, X, y, iter_step=1):
+        if self.stochGd:
+            print('Implementing Stochastic Gradient Descent.')
+            return self.__stochGrad(X, y, iter_step)
+        else:
+            print('Implementing Batch Gradient Descent.')
+            return self.__batchGrad(X, y, iter_step)
+    
+    def __batchGrad(self, X, y, iter_step=1):
+        w, X = self.init_w_x(X)
+        for i in range(self.max_iter + 1):
+            w_old = w.copy()
+            cost = self.cost(X, y, w)
+            self.cost_hist.append(cost)
+            self.w_hist.append(w)
+            w = w + self.lr * self.__calc_grad(X, y, w)
+            if i % iter_step == 0:
+                print(f'Iteration {i: 5d} | Cost: {cost: 3.3f}')
+            if self.__check_convergence(w_old, w):
+                print(f'Stopping criteria satisfied at iteration {i + 1}.')
+                break
+        self.w = w
 
+    def __stochGrad(self, X, y, iter_step=1):
+        w, X = self.init_w_x(X)
+        for i in range(self.max_iter + 1):
+            w_old = w.copy()
+            cost = self.cost(X, y, w)
+            self.cost_hist.append(cost)
+            self.w_hist.append(w)
+            for j in range(len(X)):
+                w = w + self.lr * self.__calc_grad(X[j], y[j], w)
+            if i % iter_step == 0:
+                print(f'Iteration {i: 5d} | Cost: {cost: 3.3f}')
+            if self.__check_convergence(w_old, w):
+                print(f'Stopping criteria satisfied at iteration {i + 1}.')
+                break
+        self.w = w
+
+    def __calc_grad(self, X, y, w):
+        if self.stochGd:
+            return np.dot(X.T, (y - self.sigmoid(np.dot(X, w))).item()).reshape(w.shape)
+        return np.dot(X.T, np.reshape(y,(len(y),1)) - self.sigmoid(np.dot(X, w)))
+    
     def init_w_x(self, X):
         w = np.zeros((X.shape[1] + 1, 1))
         X = np.c_[np.ones((X.shape[0], 1)), X]
@@ -19,25 +64,10 @@ class LogReg:
     
     def cost(self, X, y, w):
         z = np.dot(X, w)
-        cost0 = np.dot(y.T, np.log(self.sigmoid(z)))
-        cost1 = np.dot((1 - y).T, np.log(1 - self.sigmoid(z)))
-        cost = -(cost0 + cost1) / len(X)
+        cost1 = np.dot(y.T, np.log(self.sigmoid(z)))
+        cost0 = np.dot((1 - y).T, np.log(1 - self.sigmoid(z)))
+        cost = -(cost1 + cost0)
         return cost.item()
-    
-    def fit(self, X, y, iter_step=1):
-        w, X = self.init_w_x(X)
-        for i in range(self.max_iter + 1):
-            w_old = w.copy()
-            w = w - self.lr * np.dot(X.T, self.sigmoid(np.dot(X, w)) - np.reshape(y,(len(y),1)))
-            cost = self.cost(X, y, w)
-            self.cost_hist.append(cost)
-            self.w_hist.append(w)
-            if i % iter_step == 0:
-                print(f'Iteration {i: 5d} | Cost: {cost: 3.3f}')
-            if self.__check_convergence(w_old, w):
-                print(f'Stopping criteria satisfied at iteration {i + 1}.')
-                break
-        self.w = w
 
     def predict(self, X):
         z = np.dot(self.init_w_x(X)[1], self.w)
--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 Implementing Logistic Regression for binary classification from scratch using basic NumPy and Pandas functions.
 
 ## Plots
-![Training costs for Gradient Descent with Holdout](/plots/data_dist.png)
-![Training costs for Gradient Descent with Monte-Carlo](/plots/cost_hist.png)
-![Training costs for Gradient Descent with K-fold](/plots/confusion_matrix.png)
-![Training costs for Stochastic Gradient Descent with Holdout](/plots/data_dist_db.png)
+![Data distribution](/plots/data_dist.png)
+### Batch Gradient Descent
+![Training costs for Batch Gradient Descent](/plots/gd_cost_hist.png)
+![Confusion Matrix for Batch Gradient Descent](/plots/gd_confusion_matrix.png)
+![Data distribution along with Decision Boundary](/plots/gd_data_dist_db.png)
+### Stochastic Gradient Descent
+![Training costs for Stochastic Gradient Descent](/plots/sgd_cost_hist.png)
+![Confusion Matrix for Stochastic Gradient Descent](/plots/sgd_confusion_matrix.png)
+![Data distribution along with Decision Boundary](/plots/sgd_data_dist_db.png)
