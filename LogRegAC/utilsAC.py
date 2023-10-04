86439a5880b6797234439699aa91bfe2d5755d85
AbinashChetia <abinashdu2000@gmail.com>
2023-10-04 08:18:00 +0530

    Updated.

--- LogRegAC/utilsAC.py
+++ LogRegAC/utilsAC.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def splitTrainTest(x, y, train_ratio=0.8):
     '''
@@ -79,3 +80,42 @@ def normMinMax(df, mode='train', train_min=None, train_max=None):
         for col in data.columns:
             data[col] = (data[col] - train_min[col]) / (train_max[col] - train_min[col])
         return data
+    
+def disp_conf_mat(perf_m):
+    gd1_cfm = [[perf_m['tn'], perf_m['fn']], [perf_m['fp'], perf_m['tp']]]
+    _, ax = plt.subplots(figsize=(3, 3))
+    ax.matshow(gd1_cfm, cmap=plt.cm.Blues, alpha=0.3)
+    for i in range(len(gd1_cfm)):
+        for j in range(len(gd1_cfm[i])):
+            ax.text(x=j, y=i,s=gd1_cfm[i][j], va='center', ha='center', size='xx-large')
+    plt.xlabel('Actuals', fontsize=10)
+    plt.ylabel('Predictions', fontsize=10)
+    plt.title('Confusion Matrix', fontsize=10)
+    plt.show()
+
+def plot_dec_bound(X, y, w):
+    if X.shape[1] == 2:
+        gd_plot_y = []
+        for i in range(X.shape[0]):
+            gd_plot_y.append(-(w[0] + (w[1] * X.iloc[i, 0])) / w[2])
+        plt.figure(figsize=(5, 5))
+        plt.plot(X.iloc[:, 0], gd_plot_y, c='black')
+        plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y.map({0: 'blue', 1: 'red'}), marker='o')
+        plt.xlabel('x1')
+        plt.ylabel('x2')
+        plt.title('Decision Boundary')
+        plt.show()
+    elif X.shape[1] == 3:
+        plot_y = []
+        for i in range(X.shape[0]):
+            plot_y.append(-(w[0] + (w[1] * X.iloc[i, 0]) + (w[2] * X.iloc[i, 1])) / w[3])
+        ax = plt.axes(projection='3d')
+        ax.plot_trisurf(X.iloc[:, 0], X.iloc[:, 1], plot_y, cmap='Greens', alpha=0.7)
+        ax.scatter3D(X.iloc[:, 0], X.iloc[:, 1], X.iloc[:, 2], c=y.map({0: 'blue', 1: 'red'}), marker='o')
+        ax.set_xlabel('X1')
+        ax.set_ylabel('X2')
+        ax.set_zlabel('X3')
+        plt.title('Decision Boundary')
+        plt.show()
+    else:
+        print('Facility to plot decision boundary for data with more than 3 features has not been added yet!')
